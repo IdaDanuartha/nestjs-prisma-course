b07616e7eff686045596f4d66585158bd5065a84
IdaDanuartha <danuart21@gmail.com>
2023-11-02 16:29:05 +0800

    add docker compose

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login() {
+    return {
+      msg: "I have logged in"
+    }
+  }
+
+  signup() {
+    return {
+      msg: "I have signed up"
+    }
+  }
+}
