9b15b6c5cdacd7f81aca331a5edf3a0bd8c897e1
IdaDanuartha <danuart21@gmail.com>
2023-11-05 20:03:21 +0800

    bookmark crud controller

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
-@Controller('bookmark')
-export class BookmarkController {}
+@UseGuards(JwtGuard)
+@Controller('bookmarks')
+export class BookmarkController {
+
+  // Get all bookmarks
+  @Get('')
+  getBookmarks(@GetUser('id') userId: number) {}
+
+  // Get bookmark by id
+  @Get(':id')
+  getBookmarkById(@GetUser('id') userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {}
+
+  // Store bookmark to database
+  @Post()
+  storeBookmark(@GetUser('id') userId: number, @Body() dto: CreateBookmarkDto) {}
+
+  // Update bookmark from database
+  @Patch(':id')
+  updateBookmark(@GetUser('id') userId: number, @Body() dto: EditBookmarkDto) {}
+
+  // Destroy bookmark from database
+  @Delete(':id')
+  deleteBookmark(@GetUser('id') userId: number, @Param('id', ParseIntPipe) bookmarkId: number) {}
+}
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  
+  // Get all bookmarks  
+  getBookmarks() {}
+
+  // Get bookmark by id  
+  getBookmarkById() {}
+
+  // Store bookmark to database  
+  storeBookmark() {}
+
+  // Update bookmark from database  
+  updateBookmark() {}
+
+  // Destroy bookmark from database  
+  deleteBookmark() {}
+
+}
